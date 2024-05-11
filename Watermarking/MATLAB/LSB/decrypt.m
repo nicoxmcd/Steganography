6c66c0d89eaaba482896c26da9a5bad37c305f3a
SeongJun An <123031251+successjun@users.noreply.github.com>
2024-05-11 00:31:38 -0400

    Update decrypt.m

--- Watermarking/MATLAB/LSB/decrypt.m
+++ Watermarking/MATLAB/LSB/decrypt.m
@@ -1,1 +1,16 @@
-
+function extracted_watermark = extract_watermark(watermarked_image, key)
+    % Read the watermarked image
+    watermarked_img = imread("watermarked_image.png");
+    
+    % Extract the least significant bit (LSB) where the watermark was embedded
+    lsb = bitget(watermarked_img(:,:,1), 1);
+    
+    % Decrypt the watermark
+    decrypted_watermark = xor(lsb, 0201);
+    
+    % Convert binary image to uint8 for display
+    extracted_watermark = uint8(decrypted_watermark * 255);
+    
+    % Save or display the extracted watermark
+    imwrite(extracted_watermark, 'extracted_watermark.png');
+end
